d7ba4ccd56ff0c02b0b08dfc24902debf9084457
tinyynoob <sadpotato1920@gmail.com>
2022-03-06 20:03:24 +0800

    Add some test files and .gitignore

---
+++ counter.v
@@ -0,0 +1,21 @@
+/* obtain from https://medium.com/macoclock/running-verilog-code-on-linux-mac-3b06ddcccc55 */
+module counter(out, clk, reset);
+
+  parameter WIDTH = 8;
+
+  output [WIDTH-1 : 0] out;
+  input            clk, reset;
+
+  reg [WIDTH-1 : 0]   out;
+  wire            clk, reset;
+
+  always @(posedge clk)
+    out <= out + 1;
+
+  always @reset
+    if (reset)
+      assign out = 0;
+    else
+      deassign out;
+
+endmodule // counter
